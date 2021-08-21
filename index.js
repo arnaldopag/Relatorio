4c674b70c7a2c5a38ff0e788aea48a5b6b64962e
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-08-20 21:37:50 -0300

    arrumando a tabela

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const buttonPrint = document.querySelector('#print');
-buttonPrint.addEventListener('click', createPDF)
-
 
 function get(){
 
@@ -56,23 +53,4 @@ function getInput(){
 
   console.log(authorInput)
   console.log(titleInput)
-}
-function createPDF() {
-  var minhaTabela = document.querySelector('#myTable');
-  var style = "<style>";
-  style = style + "table {width: 100%;font: 20px Calibri;}";
-  style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
-  style = style + "padding: 2px 3px;text-align: center;}";
-  style = style + "</style>";
-  // CRIA UM OBJETO WINDOW
-  var win = window.open('', '', 'height=700,width=700');
-  win.document.write('<html><head>');
-  win.document.write('<title>Empregados</title>');   // <title> CABEÇALHO DO PDF.
-  win.document.write(style);                                     // INCLUI UM ESTILO NA TAB HEAD
-  win.document.write('</head>');
-  win.document.write('<body>');
-  win.document.write(minhaTabela);                          // O CONTEUDO DA TABELA DENTRO DA TAG BODY
-  win.document.write('</body></html>');
-  win.document.close(); 	                                         // FECHA A JANELA
-  win.print();                                                            // IMPRIME O CONTEUDO
 }
